219036bd80a4113a979905bc12bc2a36027c999e
Miguel Carhuas <mikel_ct@hotmail.com>
2025-09-20 14:53:29 -0500

    upd yml

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/*.unit.test.ts'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['json', 'lcov', 'text'],
 };
