07db2d295bec9fd46afdc2da83eb98209e4ce160
Miguel Carhuas <mikel_ct@hotmail.com>
2025-09-20 15:05:38 -0500

    create tsconfig.test.json file

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,12 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/*.unit.test.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.test.json'
+    }
+  },
   collectCoverage: true,
   coverageDirectory: 'coverage',
-  coverageReporters: ['json', 'lcov', 'text'],
+  coverageReporters: ['json', 'lcov', 'text']
 };
